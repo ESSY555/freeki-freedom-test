1d575e43948460cf4dd87f1f7cefbee7aa41bc21
ESSY555 <106964906+ESSY555@users.noreply.github.com>
2024-05-16 03:27:16 -0800

    title updated

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,7 +5,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
+    <title>Freeki-freedom</title>
     <script src="https://cdn.tailwindcss.com"></script>
     <style type="text/tailwindcss">
         @layer utilities {
